a5933b81ac2818363928317205055665469570bc
BejPastan <64369922+starababara@users.noreply.github.com>
2022-04-14 09:09:34 +0200

    Add files via upload

---
+++ Praktyki1/PHPScripts/sendMail.php
@@ -0,0 +1,48 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    require 'G:\Programowanie\vendor\autoload.php';
+    $email = new PHPMailer(TRUE);
+    
+    $sender = $_POST['email'];
+    $msg=$_POST['content'];
+
+    try
+    {
+        $email->setFrom('s.synaszko@gmail.com', $sender,0);
+        $email->addAddress('szymon.synaszko@tm1.edu.pl', 'TestowyOdbiorca');
+        $email->Subject = 'msg from website';
+        $email->Body=$msg;
+
+        $email->isSMTP();
+        $email->SMTPDebug  = 1;  
+        /* SMTP server address. */
+        $email->Host = 'smtp.gmail.com';
+        /* Use SMTP authentication. */
+        $email->SMTPAuth = TRUE;
+        
+        /* Set the encryption system. */
+        $email->SMTPSecure = 'tls';
+        
+        /* SMTP authentication username. */
+        $email->Username = 's.synaszko@gmail.com';
+        
+        /* SMTP authentication password. */
+        $email->Password = 'NiebieskieKombiWGazie';
+        
+        /* Set the SMTP port. */
+        $email->Port = 587;
+        
+        /* Finally send the mail. */
+        $email->send();
+
+    }
+    catch (Exception $e)
+    {
+    echo $e->errorMessage();
+    }
+    catch (\Exception $e)
+    {
+    echo $e->getMessage();
+    }
+?>
